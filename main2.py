1428ba271883237b67e376f851fbc0d0a0038774
MDCFCesar <mdcf@cesar.school>
2025-05-13 18:38:14 -0300

    Alterei algumas coisas que estavam erradas, e melhorei a escolha de opção do usuario, no menu principal

--- main2.py
+++ main2.py
@@ -2,8 +2,7 @@
 #função que cria um Arquivo.txt, ID = PET{nome}.txt 
 import os
 
-def menu_principal(opcao):
-  
+def menu_principal():
         print("-="*12,)
         print('MENU PRINCIPAL COM O PET')
         print("-="*12,)
@@ -12,30 +11,57 @@ def menu_principal(opcao):
         print('3. Editar Pet (Cachorro/Gato/Ave)')
         print('4. Excluir Pet (Cachorro/Gato/Ave)')
         print('5. Para Finalizar')
-        opcao = input("Digite o número da opção escolha: ")
+        print("-="*12,)
+        #tirei o input de opção que estava errado aqui e so deixei no main
+
 def addPet():
     nome = input("Digite o nome do pet: ")
     try:
         idade = int(input("Digite a idade do pet: "))
-    except ValueError as e:
-        print(f"Digite uma idade valida! (Erro: {e})")
+    except ValueError as erro: #troquei o nome da variavel e para erro
+        print(f"Digite uma idade valida! (Erro: {erro})")#mesma coisa
+        return
 
     with open(f"Pet{nome}.txt", "w", encoding="UTF-8") as file:
         file.write(f"Nome: {nome}\n")
         file.write(f"Idade: {idade}")
+
     
 def visualizarPet():
     nome = input("Digite o nome do pet: ")
     try:
         with open(f"Pet{nome}.txt", "r", encoding="UTF-8") as file:
             #if os.path.isfile(f"Pet{nome}.txt"):
-            conteudo_Pet = (f"Pet{nome}.txt".read())
+
+            #agora conteudo_Pet le o arquivo e depois printa
+            conteudo_Pet = file.read()
             print(conteudo_Pet)
-    except FileNotFoundError as e:
-        print(f"Digite uma idade valida! (Erro: {e})")
+            
+    except FileNotFoundError as erro: #troquei o nome da variavel e para erro
+        print(f"Digite uma idade valida! (Erro: {erro})")
 
 
 while True:
+    menu_principal()
+    try:
+        opcao = int(input("Digite o número da opção escolhida: "))
+    except ValueError:
+        print("Opção inválida! Digite apenas números.")
+        continue
 
-    opcao = input("Digite o número da opção escolha: ")
+    if opcao == 1:
+        addPet()
+    elif opcao == 2:
+        visualizarPet()
+    elif opcao == 3:
+        continue    #vou criar a função editar pet, o continue e apenas para rodar os testes
+        # editar_pet()
+    elif opcao == 4:
+        continue    #vou criar a função excluir pet, o continue e apenas para rodar os testes
+        # excluir_pet()
+    elif opcao == 5:
+        print("PROGRAMA ENCERRADO")
+        break
+    else:
+        print("Opção inválida!")
 
